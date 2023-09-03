caa2ce8de0d9c86630f8db20c4d6f32cd0371ac7
albetnov <ryxinfrvr@gmail.com>
2023-09-03 15:55:57 +0700

    chore: add `CustomFactoryTest`, `TypeFactoryTest`, `FilterableTest`,

---
+++ tests/Factories/CustomFactoryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use Albet\LaravelFilterable\Enums\Operators;
+use Albet\LaravelFilterable\Factories\CustomFactory;
+use Albet\LaravelFilterable\Factories\TypeFactory;
+
+it("return array of allowedOperators", function() {
+    $typeFactory = new TypeFactory();
+
+    $customFactory = new CustomFactory($typeFactory->limit([Operators::EQ]));
+
+    expect($customFactory->get())->toEqual([Operators::EQ]);
+});
--- tests/Traits/FilterableTest.php
+++ tests/Traits/FilterableTest.php
@@ -1,5 +1,37 @@
 <?php
 
-it('', function () {
+use Albet\LaravelFilterable\Tests\Stubs\Flight;
+use Albet\LaravelFilterable\Tests\Stubs\Tickets;
 
+it('can get columns from property', function () {
+    $reflection = new \ReflectionClass(Tickets::class);
+
+    $method = $reflection->getMethod('getFilterable');
+
+    $method->setAccessible(true);
+
+    $tickets = new Tickets();
+
+    $result = $method->invoke($tickets);
+
+    $prop = $reflection->getProperty('filterableColumns');
+
+    expect($result)->toBeArray()->toEqual($prop->getValue($tickets));
+});
+
+it('can get columns from function', function () {
+    $method = new \ReflectionMethod(Flight::class, 'getFilterable');
+
+    $method->setAccessible(true);
+
+    $flights = new Flight();
+
+    $result = $method->invoke($flights);
+
+    $definerMethod = new \ReflectionMethod(Flight::class, 'filterableColumns');
+    $definerMethod->setAccessible(true);
+
+    $definer = $definerMethod->invoke($flights);
+
+    expect($result)->toBeArray()->toEqual($definer);
 });
