11388d0a8303fcc1623f653e93b4889fd96be3a7
albetnov <ryxinfrvr@gmail.com>
2023-09-03 15:59:06 +0700

    chore: change `$rows` or `getRows()` to new format `filterableColumns`

--- src/Exceptions/PropertyNotExist.php
+++ src/Exceptions/PropertyNotExist.php
@@ -6,6 +6,6 @@ class PropertyNotExist extends \Exception
 {
     public function __construct()
     {
-        parent::__construct('$rows or getRows() is not exist');
+        parent::__construct('$filterableColumns or getFilterableColumns() is not exist');
     }
 }
